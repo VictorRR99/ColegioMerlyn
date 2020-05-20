83d29a32b2e47212337330cc3049d821fad599f8
Victor Roberto <55004743+VictorRR99@users.noreply.github.com>
2020-05-20 11:07:57 -0300

    Add files via upload

--- SQL/Script/APS_Colégio_Merlyn.sql
+++ SQL/Script/APS_Colégio_Merlyn.sql
@@ -11,14 +11,38 @@ CREATE TABLE Sala (
 );
 
 CREATE TABLE Aluno (
-	cd_aluno INTEGER UNIQUE NOT NULL, /* [PK] */
+	cd_aluno INTEGER UNIQUE NOT NULL,         /* [PK] */
 	pessoa_cd_pessoa INTEGER UNIQUE NOT NULL, /* [FK] */
-	cd_sala INTEGER NOT NULL /* [FK] */
+	cd_sala INTEGER NOT NULL                  /* [FK] */
 );
 
 CREATE TABLE Notas (
-	cd_nota VARCHAR(5) UNIQUE NOT NULL /* [PK] */ -- VERIFICAR SE VARCHAR
-    cd_aluno INTEGER NOT NULL, /* [FK] */
+	cd_nota VARCHAR(5) UNIQUE NOT NULL, /* [PK] */ -- VERIFICAR SE VARCHAR
+    cd_aluno INTEGER NOT NULL,          /* [FK] */
     NP1 FLOAT NOT NULL,
-    NP2 VARCHAR(5) NOT NULL -- VERIFICAR SE VARCHAR
+    NP2 VARCHAR(5) NOT NULL -- VERIFICAR SE VARCHAR																						
 );
+
+CREATE TABLE Disciplina (
+	cd_disc INTEGER UNIQUE NOT NULL, /* [PK] */
+	nm_disc VARCHAR(30) NOT NULL 	 /* [FK] */
+);
+
+CREATE TABLE Sala_disc (
+	cd_sala_disc INTEGER UNIQUE NOT NULL, /* [PK] */
+	cd_sala INTEGER NOT NULL, 			  /* [FK] */
+	cd_disc INTEGER NOT NULL              /* [FK] */
+);
+
+CREATE TABLE Professor (
+	cd_prof INTEGER UNIQUE NOT NULL,   /* [PK] */
+	cd_pessoa INTEGER UNIQUE NOT NULL, /* [FK] */
+	cd_disc INTEGER UNIQUE NOT NULL	   /* [FK] */
+);
+
+CREATE TABLE Diretor (
+	cd_dir INTEGER UNIQUE NOT NULL, /* [PK] */
+	cd_prof INTEGER UNIQUE NOT NULL /* [FK] */
+);
+
+-- CHECAR RELACIONAMENTOS DE CARDINALIDADE
