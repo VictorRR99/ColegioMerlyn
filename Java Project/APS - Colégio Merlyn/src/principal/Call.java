fead45ab3a8db798998edccf6b8ecd8f8aab22c0
Victor Roberto <victoracc2001@hotmail.com>
2020-06-04 22:33:21 -0300

    Alterações de Teste de Serialização | Leve alteração no Modelo Lógico

--- Java Project/APS - Colégio Merlyn/src/principal/Call.java
+++ Java Project/APS - Colégio Merlyn/src/principal/Call.java
@@ -6,36 +6,39 @@ import java.util.List;
 public class Call {
 	
 	public static void main(String[] args) throws ClassNotFoundException {
+
+		/* Serialization Test*/
+		Aluno victor = new Aluno();
+		Aluno victor = new Aluno();
+		Aluno victor = new Aluno();
+		Aluno victor = new Aluno();
+		Aluno victor = new Aluno();
+		List<Pessoa>
 		
-//		try {
-//			@SuppressWarnings("unused")
-//			Connection conexao = Conexao.getConnection();
-//		} catch (ClassNotFoundException | SQLException e) {
-//			e.printStackTrace();
+		/* Original */
+        
+//		Aluno victor = new Aluno("Victor", "08618351903", "6021469", "22/02/2020");
+//		
+//		System.out.println("\n*Quantidade de Pessoas");
+//		System.out.println(victor.getPessoaCount());
+//		
+//		System.out.println("\n===============");
+//		
+//		//All Pessoas
+//		System.out.println("\n*Nome de todas as pessoas cadastradas na Escola");
+//		List<String> allPessoas = new ArrayList<String>();
+//		
+//		allPessoas.addAll(victor.getAllPessoa());
+//		
+//		for(String x : allPessoas) {
+//			System.out.println(x);
 //		}
-			
-		Aluno victor = new Aluno("Victor", "08618351903", "6021469", "22/02/2020");
-		
-		System.out.println("\n*Quantidade de Pessoas");
-		System.out.println(victor.getPessoaCount());
-		
-		System.out.println("\n===============");
-		
-		//All Pessoas
-		System.out.println("\n*Nome de todas as pessoas cadastradas na Escola");
-		List<String> allPessoas = new ArrayList<String>();
-		
-		allPessoas.addAll(victor.getAllPessoa());
-		
-		for(String x : allPessoas) {
-			System.out.println(x);
-		}
-		//All Pessoas
-		
-		System.out.println("\n===============");
-		
-		System.out.println("\n*Data de Nascimento de: " + victor.getNome());
-		System.out.println(victor.getPessoaDtNasc());
+//		//All Pessoas
+//		
+//		System.out.println("\n===============");
+//		
+//		System.out.println("\n*Data de Nascimento de: " + victor.getNome());
+//		System.out.println(victor.getPessoaDtNasc());
 		
 	}
 	
--- Java Project/APS - Colégio Merlyn/src/principal/Pessoa.java
+++ Java Project/APS - Colégio Merlyn/src/principal/Pessoa.java
@@ -1,5 +1,11 @@
 package principal;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import java.sql.Connection;
@@ -26,7 +32,53 @@ public abstract class Pessoa implements Serializable{
 		this.dtNasc = dtNasc;
 	}
 	
+	/* Serialization Handler */
+	
+	public static void saveObjectList(List<Pessoa> lista, String nomeArq) {
+	      File arq = new File(nomeArq);
+	      try {
+	    	  arq.delete();
+	    	  arq.createNewFile();
+	    
+	    	  ObjectOutputStream objOutput = new ObjectOutputStream(new FileOutputStream(arq));
+	        
+	    	  objOutput.writeObject(lista);
+	    	  objOutput.close();
+	    
+	      } catch(IOException erro) {
+	    	  System.out.printf("Erro: %s", erro.getMessage());
+	      }
+	}
+	
+	
+	@SuppressWarnings("all")
+	public static ArrayList<Pessoa> readObjectList(String nomeArq) {
+		
+		ArrayList<Pessoa> lista = new ArrayList<Pessoa>();
+		try {
+			File arq = new File(nomeArq);
+			if (arq.exists()) {
+				ObjectInputStream objInput = new ObjectInputStream(new FileInputStream(arq));
+				lista = (ArrayList<Pessoa>)objInput.readObject();
+				objInput.close();
+			}
+			
+		} catch(IOException erro1) {
+			System.out.printf("Erro: %s", erro1.getMessage());
+		} catch(ClassNotFoundException erro2) {
+			System.out.printf("Erro: %s", erro2.getMessage());
+		}
+	    
+		return(lista);
+	}
+	
+	//----
+	
+	
+	//----
 	/* Métodos de Relatório [Pesquisa] JAVA*/
+	/* Métodos de Relatório [Pesquisa] JAVA */
+	
 	//Talvez fazer métodos que procuram igual a pesquisa do SQL
 	//Para tal realmente é possível a necessidade de umaa lista estática
 	//Porém haverá complicações a respeito de como será chamado os métodos
@@ -47,7 +99,7 @@ public abstract class Pessoa implements Serializable{
 		return dtNasc;
 	}
 
-	/* Métodos de Relatório [Pesquisa] PostgreSQL*/
+	/* Métodos de Relatório [Pesquisa] PostgreSQL */
 	
 	//Quantidade de Pessoas que estão cadastradas na Escola
 	public int getPessoaCount() throws ClassNotFoundException {
