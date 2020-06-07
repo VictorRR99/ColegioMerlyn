80d5e2901903392fd0273f81455ac241552b9ae1
Victor Roberto <victoracc2001@hotmail.com>
2020-06-06 22:06:26 -0300

    Alteração Menor Serialization

--- Java Project/APS - Colégio Merlyn/src/principal/Disciplina.java
+++ Java Project/APS - Colégio Merlyn/src/principal/Disciplina.java
@@ -1,8 +1,71 @@
 package principal;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Disciplina {
 
+	private static List<Disciplina> listaDisciplinas = new ArrayList<>();
+	
 	Disciplina(){
 		
 	}
+	
+	/* Serialization Handler */
+
+	public static void serialization() {
+		Disciplina.saveObjectList(listaDisciplinas, "Disciplinas");
+	}
+	
+	public static void desserialization() {
+		listaDisciplinas = Disciplina.readObjectList("Disciplinas");
+	}
+	
+	private static void saveObjectList(List<Disciplina> lista, String nomeArq) {
+	      File arq = new File(nomeArq);
+	      try {
+	    	  arq.delete();
+	    	  arq.createNewFile();
+	    
+	    	  ObjectOutputStream objOutput = new ObjectOutputStream(new FileOutputStream(arq));
+	        
+	    	  objOutput.writeObject(lista);
+	    	  objOutput.close();
+	    
+	      } catch(IOException erro) {
+	    	  System.out.printf("Erro: %s", erro.getMessage());
+	      }
+	}
+	
+	@SuppressWarnings("all")
+	private static List<Disciplina> readObjectList(String nomeArq) {
+		
+		ArrayList<Disciplina> lista = new ArrayList<Disciplina>();
+		
+		try {
+			File arq = new File(nomeArq);
+			if (arq.exists()) {
+				ObjectInputStream objInput = new ObjectInputStream(new FileInputStream(arq));
+				lista = (ArrayList<Disciplina>) objInput.readObject();
+				objInput.close();
+			}
+			
+		} catch(IOException erro1) {
+			System.out.printf("Erro: %s", erro1.getMessage());
+		} catch(ClassNotFoundException erro2) {
+			System.out.printf("Erro: %s", erro2.getMessage());
+		}
+	    
+		return(lista);
+		
+	}
+	
+	
+	
 }
---
+++ Java Project/APS - Colégio Merlyn/src/principal/Sala.java
@@ -0,0 +1,72 @@
+package principal;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class Sala implements Serializable {
+	
+	private static List<Sala> listaSalas = new ArrayList<>();
+	
+	Sala(){
+		
+	}
+	
+	/* Serialization Handler */
+
+	public static void serialization() {
+		Sala.saveObjectList(listaSalas, "Salas");
+	}
+	
+	public static void desserialization() {
+		listaSalas = Sala.readObjectList("Salas");
+	}
+	
+	private static void saveObjectList(List<Sala> lista, String nomeArq) {
+	      File arq = new File(nomeArq);
+	      try {
+	    	  arq.delete();
+	    	  arq.createNewFile();
+	    
+	    	  ObjectOutputStream objOutput = new ObjectOutputStream(new FileOutputStream(arq));
+	        
+	    	  objOutput.writeObject(lista);
+	    	  objOutput.close();
+	    
+	      } catch(IOException erro) {
+	    	  System.out.printf("Erro: %s", erro.getMessage());
+	      }
+	}
+	
+	@SuppressWarnings("all")
+	private static List<Sala> readObjectList(String nomeArq) {
+		
+		ArrayList<Sala> lista = new ArrayList<Sala>();
+		
+		try {
+			File arq = new File(nomeArq);
+			if (arq.exists()) {
+				ObjectInputStream objInput = new ObjectInputStream(new FileInputStream(arq));
+				lista = (ArrayList<Sala>) objInput.readObject();
+				objInput.close();
+			}
+			
+		} catch(IOException erro1) {
+			System.out.printf("Erro: %s", erro1.getMessage());
+		} catch(ClassNotFoundException erro2) {
+			System.out.printf("Erro: %s", erro2.getMessage());
+		}
+	    
+		return(lista);
+		
+	}
+	
+	
+}
